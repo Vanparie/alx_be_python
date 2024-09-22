9616a434108a3edbee77df631358b09108c191e4
Davidson Leparie <davidsonleparie@gmail.com>
2024-09-22 15:42:06 +0300

    Created a daily reminder program

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,13 +11,15 @@ match priority:
     case "medium":
         reminder = f"'{task}' is a medium priority. Consider attending to it as soon as you can."
     case "low":
-        reminder = f"'{task}' is a low priority. Consider completing it when you have free time."
+        reminder = f"'{task}' is a low priority."
     case _:
         reminder = "Invalid priority level. Please enter high, medium, or low."
 
 # Check if the task is time-bound and modify the reminder
 if time_bound == "yes":
     reminder += " This requires immediate attention today!"
+else:
+    reminder += "Take your time. This is not urgent"
 
 # Provide the customized reminder
 print(reminder)
