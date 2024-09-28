9b2a8800f1bd1d03a0ed7df4930837d6e40abadd
Davidson Leparie <davidsonleparie@gmail.com>
2024-09-28 21:05:25 +0300

    Added a temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Define Global Conversion Factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5 / 9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9 / 5)
 CELSIUS_OFFSET = 32  # For Fahrenheit to Celsius and vice versa
 
 
