2605987e2706d7ddb060d8b7f290cbbcd6c5af25
Davidson Leparie <davidsonleparie@gmail.com>
2024-10-11 19:33:05 +0300

    Added two files to the oop directory

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"{self.title} by {self.author}"
 
 
 class EBook(Book):
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{super().__str__()} - Size: {self.file_size} KB"
+        return f"Ebook: {super().__str__()}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -22,7 +22,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().__str__()} - Pages: {self.page_count}"
+        return f"PrintBook: {super().__str__()},  Page Count: {self.page_count}"
 
 
 class Library:
@@ -33,6 +33,6 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        print("Books in the Library:")
+        print("Books: ")
         for book in self.books:
             print(book)
